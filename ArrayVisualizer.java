2577a2dde9b6022202015ceb346ef88211990fb2
davidbang <davidbang2012@gmail.com>
2008-09-10 13:59:18 -0400

    Update ArrayVis

--- ArrayVisualizer.java
+++ ArrayVisualizer.java
@@ -1,45 +1,61 @@
 import java.util.*;
+import java.lang.*;
+import java.lang.reflect.Array;
 
-public class ArrayVisualizer {
+
+public class ArrayVisualizer <T> {
     T [] vis;
     private int slot;
     
-    public ArrayVisualizer (int size) {
-	vis = new T [size];
+    public ArrayVisualizer (T type, int size) {
+	@SuppressWarnings("unchecked")
+	vis = (T[]) Array.newInstance (type , size);
 	slot = 0;
     }
     
-    public String toString () {
+    public void print () {
 	System.out.print ( "[ ");
-	for (T var : vis) {
-	    System.out.print ( "," + var); //fix at home
+	for (int i = 0; i < vis.length - 1; i ++ ) {
+	    if (vis [i] == null) 
+		System.out.print ( " ,");
+	    else
+		System.out.print ( vis [i] + ","); 
 	}
+	if (vis [vis.length - 1] == null) 
+	    System.out.print ( " ]");
+	else
+	    System.out.print ( vis [vis.length - 1] + " ]");
     }
 
-   public void add (T stuff) {
+    public void add (T stuff) {//adds to next empty slot in array
 	try{
+	    while (vis [slot] != null) 
+		slot ++;
 	    vis [slot] = stuff;
 	    slot ++;
-
 	}
-	catch (IndexOutOfBoundException e) {
+	catch (Exception e) {
 	    System.out.println (e.getMessage() + ": Array is full");
 	}
     }
    
-    public void delete () {
+    public void delete () {//deletes rightmost value in array
 	try{
-	    vis [index] = null;
+	    while (vis [slot] == null) 
+		slot --;
+	    vis [slot] = null;
+	    slot --;
 	}
-	catch (IndexOutOfBoundException e) {
+	catch (Exception e) {
 	    System.out.println (e.getMessage());
 	}
+    }
     
     public void insert (int index,  T stuff) {
 	try{
 	    vis [index] = stuff;
 	}
-	catch (IndexOutOfBoundException e) {
+	catch (Exception e) {
 	    System.out.println (e.getMessage());
 	}
     }
@@ -48,7 +64,7 @@ public class ArrayVisualizer {
 	try{
 	    vis [index] = null;
 	}
-	catch (IndexOutOfBoundException e) {
+	catch (Exception e) {
 	    System.out.println (e.getMessage());
 	}
     }
