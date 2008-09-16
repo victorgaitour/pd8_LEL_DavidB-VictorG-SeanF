cf7bb4fe6d12604cd059982d32683c641e7f7557
davidbang <davidbang2012@gmail.com>
2008-09-16 14:00:31 -0400

    BST

--- BSTVisualizer.java
+++ BSTVisualizer.java
@@ -4,6 +4,7 @@ import java.lang.*;
 
 public class BSTVisualizer <T> {
     ArrayList <T> tree = new ArrayList <T> ();
+    int size = 0;
     
     public void add (T data) {
     }
@@ -14,6 +15,40 @@ public class BSTVisualizer <T> {
     public String toString () {
 	String retStr = "";
 	int parse = 1;
-	while (parse
-	for (int i = parse; i > 0; i--) {
+	int index = 0;
+	boolean dash = false;
+	int height = (int) (Math.log(size)/Math.log(2) * 2);
+	while (index < tree.size()) { 
+	    if (dash) {
+		for (int i = height; height > 0; i --) {
+		    retStr += " ";
+		}
+		for (int i = parse; i > 0; i--) {
+		    retStr += "/  \\  ";
+		}
+		dash = false;
+	    }
+	
+	    else{
+		for (int i = parse; i > 0; i--) {
+		    if (index < tree.size ()) {
+			retStr = retStr + tree.get (index) + "  ";
+			index ++;
+		    }
+		    else
+			break;		 
+		}
+		dash = true;
+		parse = parse * 2;
+	    }
+	    retStr += "\n";
+	    height--;
+	}
+	return retStr;
+    }
+
+}
+	    
+	    
+
 	    
