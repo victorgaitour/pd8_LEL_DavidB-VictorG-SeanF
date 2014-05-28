9cb392f1a5463a43e6b2a65a1b54b9376ee6f016
Victor Gaitour <victorgaitour@yahoo.com>
2014-05-28 13:53:44 -0400

    lel

--- ArrayListVisualizer.java
+++ ArrayListVisualizer.java
@@ -12,6 +12,16 @@ public class ArrayListVisualizer<T>{
 	System.out.print(ESC + "2J"); //clear the screen
 	System.out.println(arraylist); 
     }
+    public void insert(int index, T stuff){
+	try {
+	    arraylist.set(index,stuff);
+	    System.out.print(ESC+"2J");
+	    System.out.println(arraylist);
+	}
+	catch (Exception e){
+	    System.out.println(e.getMessage() + ": Invalid index. Try again");
+	}
+    }
     public void delete(int index){
 	arraylist.remove(index);
 	System.out.print(ESC + "2J"); //clear the screen
