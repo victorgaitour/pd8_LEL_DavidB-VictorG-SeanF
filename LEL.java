c41901824d9c7d2b33108d8c65e4a144bda34d59
Sean Fitzgerald <seanfitzgerald321@gmail.com>
2014-05-29 14:00:39 -0400

    LEL

--- LEL.java
+++ LEL.java
@@ -1,6 +1,9 @@
 //Sean Fitzgerald, David Bang, Victor Gaitour
 //Driver file
 
+import java.io.*;
+import java.util.*;
+
 public class LEL {
     
     public static void main (String[] args) {
@@ -9,7 +12,7 @@ public class LEL {
 	Scanner sc = new Scanner( System.in );
 	String inputData = "";
 	try {
-	    String choice1 = sc.nextInt();
+	    int choice1 = sc.nextInt();
 	    if ( choice1 == 1 ) {
 		System.out.print( "Your file name: " );
 		try {
@@ -27,10 +30,11 @@ public class LEL {
 		inputData = sc.nextLine();
 	    }
 	    else
-		System.out.println( "ERROR: User input invalid" );
+		System.out.println( "ERROR: Please input a valid int" );
 	}
-	catch (Exception e)
-	    System.out.println( "ERROR: User input invalid" );
+	catch (Exception e) {
+	    System.out.println( "ERROR: Please input a valid int" );
+	}
 
 	ArrayList data = new ArrayList<String>();
 	for ( int i = 0; i < inputData.length(); i++ ) {
@@ -41,6 +45,64 @@ public class LEL {
 	    }
 	    i++; //skips comma
 	    data.add( value );
+	}
+
+	//^^^ENTERING DATA CODE^^^
+	
+	System.out.println( "What kind of data structure do you want to visualize?\n1: Array\n2: ArrayList\n3: Binary Search Tree\n4: Heap\n5: Stack\n6: Linked List\n7: Queue\n8: Graph" );
+	System.out.print( "Choice: " );
+
+	try {
+	    int choice2 = sc.nextInt();
+	    if ( choice2 == 1 ) { //Array
+	    
+	    }
+	    else if ( choice2 == 2 ) { //ArrayList
+
+	    }
+	    else if ( choice2 == 3 ) { //BST
+
+	    }
+	    else if ( choice2 == 4 ) { //Heap
+
+	    }
+	    else if ( choice2 == 5 ) { //Stack
+
+	    }
+	    else if ( choice2 == 6 ) { //LList
+
+	    }
+	    else if ( choice2 == 7 ) { //Queue
+
+	    }
+	    else if ( choice2 == 8 ) { //Graph
+		System.out.println( "Please choose a type of graph.\n1: Bar Graph\n2: Line Graph\n3: Pie Chart" );
+		int graphChoice;
+		String graphTitle = "";
+		String xTitle = "";
+		String yTitle = "";
+		try {
+		    graphChoice = sc.nextInt();
+		    System.out.print( "Enter a graph title: " );
+		    graphTitle = sc.nextLine();
+		    if ( graphChoice == 1 || graphChoice == 2 ) { //Bar or Line graph
+			System.out.print( "Enter a x-axis title: " );
+			xTitle = sc.nextLine();
+			System.out.print( "Enter a y-axis title: " );
+			yTitle = sc.nextLine();
+		    }
+		    else if ( graphChoice != 3 )
+			System.out.println( "ERROR: Please enter a valid int" );
+		}
+		catch ( Exception e ) {
+		    System.out.println( "ERROR: Please input a valid int" );
+		}
+	    }
+	    else
+		System.out.println( "ERROR: User input invalid" );
+	}
+	catch ( Exception e ) {
+	    System.out.println( "ERROR: Please input a valid int" );
 	}
     }
 }
