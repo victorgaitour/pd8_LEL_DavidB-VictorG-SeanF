d8c1bf560ec572908276b767cccbe63aa6bfdbec
Sean Fitzgerald <seanfitzgerald321@gmail.com>
2014-06-04 13:57:30 -0400

    began piechart

---
+++ PieChart.java
@@ -0,0 +1,22 @@
+
+import org.jfree.*;
+
+public class PieChart extends JFrame {
+
+    DefaultPieDataset data;
+    String title;
+
+    public PieChart( String t ) {
+	data = new DefaultPieDataset();
+	title = t;
+    }
+
+    public void addValue( String s ) {
+	pieDataset.setValue( s, Integer.parseInt(s) );
+    }
+
+    public JFreeChart createChart() {
+	System.out.println( "Creating chart..." );
+	PiePlot plot = new PiePlot( data );
+    }
+}
