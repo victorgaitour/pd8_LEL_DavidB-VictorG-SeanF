d9309a995ec1391fe3ec8aac1008ad5df6cceccb
Victor Gaitour <victorgaitour@yahoo.com>
2014-06-04 13:41:11 -0400

    Arraylist fixed, now working along with driver for testing arraylist

--- ArrayListVisualizer.java
+++ ArrayListVisualizer.java
@@ -1,23 +1,23 @@
 import java.util.*;
 import java.io.*;
 
-public class ArrayListVisualizer<T>{
+public class ArrayListVisualizer{
     final static String ESC = "\033[";
-    private ArrayList<T> arraylist;
-    public ArrayListVisualizer(ArrayList<T> lel){
+    private ArrayList<Integer> arraylist;
+    public ArrayListVisualizer(ArrayList<Integer> lel){
 	arraylist=lel;
     }
     public ArrayListVisualizer(){
-	arraylist= new ArrayList<T>();
+	arraylist= new ArrayList<Integer>();
     }
-    public void add(T stuff){
+    public void add(int stuff){
 	arraylist.add(stuff); //clear the screen
-	System.out.println(arraylist); 
+	System.out.println(toString()); 
     }
-    public void insert(int index, T stuff){
+    public void insert(int index, int stuff){
 	try {
 	    arraylist.set(index,stuff);
-	    System.out.println(arraylist);
+	    System.out.println(toString());
 	}
 	catch (Exception e){
 	    System.out.println(e.getMessage() + ": Invalid index. Try again");
@@ -25,42 +25,38 @@ public class ArrayListVisualizer<T>{
     }
     public void delete(int index){
 	arraylist.remove(index); //clear the screen
-	System.out.println(arraylist);
+	System.out.println(toString());
      
     }
     public String toString()
     {
-	String lel=ESC + "2J";
-	for (int i=0; i<arraylist.size();i++){
-	    if (i<10){//for spacing in single digits indeces
-		lel=lel + i + "  ";
-	 	T j=arraylist.get(i);
-		try{
-		    while(j.compareTo( (Integer)10)>=0){ //for spacing
-			lel=lel+" ";
-			j=j/10;
-		    }
-		}
-		catch (Exception e){
-		    while(j.length>=2){
-			lel=lel+" ";
-			j=j.substring(0,j.length-1);
-		    }
-		}
-	    }
-	    else {//for spacing in double digits
-		lel=lel+i;
-		T j=arraylist.get(i);
-		while(j>=10){
-		    lel=lel+" ";
-		    j=j/10;
-		}
-	    }
-	}
-	
-	String retstr="Arraylist: " + arraylist.toString()+"\n";
-	retstr+="Index: "+ lel+ "\n";
-	return retstr;
-    }//O(?)
+	 String retstr=ESC + "2J";
+	 String lel="";
+	 for (int i=0; i<arraylist.size();i++){
+	     if (i<10){//for spacing in single digits
+		 lel=lel + i + "  ";
+		 int j=arraylist.get(i);
+		 while(j>=10){ //for spacing
+		     lel=lel+" ";
+		     j=j/10;
+		 }
+	     }
+	     else {
+		 if (i==10){
+		     lel=lel.substring(0,lel.length());//for spacing in double digits
+		 }
+		 lel=lel+i+" ";
+		 int j=arraylist.get(i);
+		 while(j>=10){
+		     lel=lel+" ";
+		     j=j/10;
+		 }
+	     }
+	 }
+	 
+	 retstr+="Arraylist: " + arraylist.toString()+"\n";
+	 retstr+="Index:      "+ lel+ "\n";
+	 return retstr;
+    }
 }
 
