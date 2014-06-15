9667d58f817f66a63ccb9ef904f27ecfc41c1e8b
davidbang <davidbang2012@gmail.com>
2014-06-15 19:07:28 -0400

    LEL update

--- LEL.java
+++ LEL.java
@@ -33,6 +33,12 @@ public class LEL {
 	    guiFrame.setTitle(structure + " Visualizer");
 	    guiFrame.setSize(800,800);
 
+	    try { 
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    }
+
 	    JPanel panel = new JPanel();
 
 	    
@@ -141,59 +147,7 @@ public class LEL {
 	
     
     public static void main (String[] args) {
-	
-	System.out.print( "Would you like to: 1) Read in a .csv file (in the same directory as this java file) with your data 2) Enter your data via the terminal?\n1 or 2: " );
 	Scanner sc = new Scanner( System.in );
-	String inputData = "";
-
-	try {
-	    int choice1 = sc.nextInt();
-	    if ( choice1 == 1 ) {
-		try {
-		    System.out.print( "Your file name: " );
-		    String filename = sc.next();
-		    System.out.println( "Your file name: " + filename );
-		    BufferedReader reader = new BufferedReader( new FileReader( filename ) );
-		    String line = reader.readLine();
-		    while ( line != null ) {
-			//System.out.println( "readLine: " + line );
-			inputData += line + ",";
-			line = reader.readLine();
-		    }
-		    inputData = inputData.substring( 0, inputData.length()-1 );
-		}
-		catch (Exception e) {
-		    System.out.println( "ERROR: file not found" );
-		}
-	    }
-	    else if ( choice1 == 2 ) {
-		System.out.print( "Enter your data (FORMAT: Label1:1,Label2:2 (NO SPACES)) : " );
-		inputData = sc.next();
-	    }
-	    else
-		System.out.println( "ERROR: Please input a valid int" );
-	}
-	catch (Exception e) {
-	    System.out.println( "ERROR: Please input a valid int" );
-	}
-
-	System.out.println( "inputData: " + inputData );
-	ArrayList<String> data = new ArrayList<String>();
-	for ( int i = 0; i < inputData.length(); i++ ) {
-	    String value = "";
-	    while ( i < inputData.length() && !(inputData.substring( i, i+1 ).equals(",") || inputData.substring( i, i+1 ).equals("")) ) { //isn't comma or end of string
-		value += inputData.substring( i, i+1 );
-		//System.out.println( "Value: " + value );
-		i++;
-	    }
-	    //i++; //skips comma
-	    //System.out.println( "value: " + value );
-	    data.add( value );
-	    //System.out.println( data );
-	}
-
-	System.out.println( "data: " + data );
-	//^^^ENTERING DATA CODE^^^
 	boolean notvalid = true;
 	while (notvalid) {
 	    System.out.println( "What kind of data structure do you want to visualize?\n1: Array\n2: ArrayList\n3: Binary Search Tree\n4: Heap\n5: Stack\n6: Linked List\n7: Queue\n8: Graph" );
@@ -418,6 +372,59 @@ public class LEL {
 
 		}
 		else if ( choice2 == 8 ) { //Graph
+		    	
+		    System.out.print( "Would you like to: 1) Read in a .csv file (in the same directory as this java file) with your data 2) Enter your data via the terminal?\n1 or 2: " );
+		    String inputData = "";
+
+		    try {
+			int choice1 = sc.nextInt();
+			if ( choice1 == 1 ) {
+			    try {
+				System.out.print( "Your file name: " );
+				String filename = sc.next();
+				System.out.println( "Your file name: " + filename );
+				BufferedReader reader = new BufferedReader( new FileReader( filename ) );
+				String line = reader.readLine();
+				while ( line != null ) {
+				    //System.out.println( "readLine: " + line );
+				    inputData += line + ",";
+				    line = reader.readLine();
+				}
+				inputData = inputData.substring( 0, inputData.length()-1 );
+			    }
+			    catch (Exception e) {
+				System.out.println( "ERROR: file not found" );
+			    }
+			}
+			else if ( choice1 == 2 ) {
+			    System.out.print( "Enter your data (FORMAT: Label1:1,Label2:2 (NO SPACES)) : " );
+			    inputData = sc.next();
+			}
+			else
+			    System.out.println( "ERROR: Please input a valid int" );
+		    }
+		    catch (Exception e) {
+			System.out.println( "ERROR: Please input a valid int" );
+		    }
+
+		    System.out.println( "inputData: " + inputData );
+		    ArrayList<String> data = new ArrayList<String>();
+		    for ( int i = 0; i < inputData.length(); i++ ) {
+			String value = "";
+			while ( i < inputData.length() && !(inputData.substring( i, i+1 ).equals(",") || inputData.substring( i, i+1 ).equals("")) ) { //isn't comma or end of string
+			    value += inputData.substring( i, i+1 );
+			    //System.out.println( "Value: " + value );
+			    i++;
+			}
+			//i++; //skips comma
+			//System.out.println( "value: " + value );
+			data.add( value );
+			//System.out.println( data );
+		    }
+
+		    System.out.println( "data: " + data );
+		    //^^^ENTERING DATA CODE^^^
+
 		    System.out.println( "Please choose a type of graph.\n1: Bar Graph\n2: Line Graph\n3: Pie Chart" );
 		    int graphChoice;
 		    String graphTitle = "";
